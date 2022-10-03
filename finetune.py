c74fe0f9833082ddb7b4d8a81de0243d7efc3e08
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-10-03 19:51:55 +0900

    fix

--- finetune.py
+++ finetune.py
@@ -41,7 +41,7 @@ def setup_hyperparameters():
     parser.add_argument('--source_max_length', type=int, default=None)
     parser.add_argument('--target_max_length', type=int, default=None)
     parser.add_argument('--max_epochs', type=int, default=4)
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--num_workers', type=int, default=4)
     parser.add_argument('--gradient_accumulation_steps', type=int, default=1)
     parser.add_argument('--learning_rate', type=float, default=3e-4)
