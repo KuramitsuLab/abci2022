92384ae9cf853de04056b893a81bd426da9e26d9
Nao Sohma <m1916045sn@ug.jwu.ac.jp>
2022-10-05 17:59:22 +0900

    pip追加

--- music/music.sh
+++ music/music.sh
@@ -10,6 +10,9 @@
 source /etc/profile.d/modules.sh
 module load gcc/9.3.0 python/3.8 cuda/11.2 cudnn/8.1
 
+pip3 install --user --upgrade pip
+pip3 install -r requirements.txt
+
 export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib:/apps/centos7/python/3.8.7/lib
 
 python3 finetune.py\
